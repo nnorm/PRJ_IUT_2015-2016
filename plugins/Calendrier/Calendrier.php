322f4bacabdd819dde1aac141dba17f4610e81ff
nnorm <nnorm0145@gmail.com>
2015-11-05 11:43:40 +0100

    Test plugin first file added

---
+++ plugins/Calendrier/Calendrier.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Classe du plugin calendrier
+ */
+class Calendrier_Plugin
+{
+
+  function __construct(argument)
+  {
+    /* le constructeur, à voir comment c'est structuré */
+  }
+
+  /* Des fonctions pour manipuler le calendrier, en relation avec la BDD */
+}
